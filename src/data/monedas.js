3e1d620739e1db34df91b8c34d5c44d11204fb00
JosueReyes-1 <josueisrae222@gmail.com>
2023-05-26 23:28:28 -0600

    integracion del proyecto terminado

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id: "USD", nombre: "Dolar de Estados Unidos" },
+    { id: "MNX", nombre: "Peso Mexicano" },
+    { id: "EUR", nombre: "EURO" },
+    { id: "GBP", nombre: "Libra Esterlina" },
+];
+
+export {
+    monedas
+}
